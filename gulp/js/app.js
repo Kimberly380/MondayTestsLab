e2d1e7e44767c9d4483874a97fb9fe9c86410572
Yvonne Hayes <queyvonne@gmail.com>
2016-03-21 20:23:28 -0700

    Most of Problem6 except textContent paragraphs

--- gulp/js/app.js
+++ gulp/js/app.js
@@ -1,6 +1,16 @@
-var pAddFiveIn  = document.getElementById('pAddFiveIn');
-var pAddFiveOut = document.getElementById('pAddFiveOut');
 
-var arg = 10;
-pAddFiveIn.textContent  = 'Calling addFive() with an argument of ' + arg;
-pAddFiveOut.textContent = 'addFive() returned ' + addFive(arg);
+
+
+
+var Q1 = document.getElementById('Question1');
+var A1 = document.getElementById('Answer1');
+var Q2 = document.getElementById('Question2');
+var A2 = document.getElementById('Answer2');
+var Q3 = document.getElementById('Question3');
+var A3 = document.getElementById('Answer3');
+var Q4 = document.getElementById('Question4');
+var A4 = document.getElementById('Answer4');
+var Q5 = document.getElementById('Question5');
+var A5 = document.getElementById('Answer5');
+
+Answer1.textContent = "Hi there!"
